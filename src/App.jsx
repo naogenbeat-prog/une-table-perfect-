5462ef253fe98dc7f9bd2247f310bfe3c096cfa7
naogenbeat-prog <naogenbeat@gmail.com>
2026-03-25 22:51:23 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,16 @@ const CONFIG = {
 };
 
 const images = {
-  concept1: "/concept-img-2.JPG",
+  concept1: "/business-2.png",
   serviceWedding: "/cocktail-2.png",
-  serviceCorporate: "/business-2.png",
+  serviceCorporate: "/cocktail-7.png",
   servicePrivate: "/private-1.png",
   gallery:[
-    "/roastbeef-2.png",
+    "/business-4.jpg",
     "/business-1.jpeg",
     "/concept-img.png",
-    "https://images.unsplash.com/photo-1530103043960-ef38714abb15?auto=format&fit=crop&q=80&w=800",
-    "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?auto=format&fit=crop&q=80&w=800",
+    "/cocktail-3.png",
+    "/business-9.jpeg",
   ],
 };
 
